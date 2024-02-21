0528e55796bba6d6172f7623a18fa8e9193c6300
Riiyaz <45689131+abdulriiyaz@users.noreply.github.com>
2024-02-21 19:50:59 +0530

    Updoots: Implemented:=> MySelect, MyWhere, MyFirst.

--- Program.cs
+++ Program.cs
@@ -17,9 +17,7 @@
         public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> that, Func<TSource, TResult> projection)
         {
             foreach (var item in that)
-            {
                 yield return projection(item);
-            }
         }
 
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> that, Func<T, bool> predicate)
@@ -27,64 +25,54 @@
             foreach (var item in that)
             {
                 if (predicate(item))
-                {
                     yield return item;
-                }
             }
         }
 
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> projection)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            foreach (var item in source)
+            {
+                yield return projection(item);
+            }
+        }
+
+        public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                    yield return item;
+            }
+        }
+        public static TSource MyFirst<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("Source");
+
+            return source.First();
+        }
     }
     class Program
     {
         public static void Main(string[] args)
         {
-            List<Person> ppl = new List<Person>
-            {
-                new Person("John", "Doe"),
-                new Person("Jane", "Doe"),
-                new Person("John", "Smith"),
-            };
-            ppl.Map(p => p._firstName).ToList().ForEach(Console.WriteLine);
-            ppl.Filter(p => p._firstName == "John").ToList().ForEach(Console.WriteLine);
+            //use MySelect
+            var num = new List<int> { 1, 2, 3, 4, 5 };
 
-            //var fPPl = ppl
-            //    .Filter(p => p._firstName.EndsWith("N", StringComparison.CurrentCultureIgnoreCase))
-            //    .Map(p => p._firstName);
+            num.MySelect(x => x * 2).ToList().ForEach(x => Console.WriteLine(x));
 
+            num.MyWhere(x => x == 5).ToList().ForEach(x => Console.WriteLine(x));
 
-            ////METHOD SYNTAX
-            //var fPPl = ppl
-            //   .Where(p => p._firstName.EndsWith("N", StringComparison.CurrentCultureIgnoreCase))
-            //   .Select(p => p._firstName);
-
-
-            ////QUERY SYNTAX
-            //var fPPl2 =
-            //    from p in ppl
-            //    where p._firstName.EndsWith("N", StringComparison.CurrentCultureIgnoreCase)
-            //    select p._firstName;
-
-
-            ////Enumerable Class has static methods to use.
-            //var num = Enumerable.Range(0, 99)
-            //    .Where(n => n % 2 == 0)
-            //    .Select(n => n * n);
-
-            ////instantiate empty int enumerble
-            //var num2 = Enumerable.Empty<int>();
-
-            ////instantiate enumerable with single element
-            Enumerable.Repeat("Riyaz", 20)
-                .ToList()
-                .ForEach(Console.WriteLine);
-
-
-
-            //fPPl.ToList().ForEach(Console.WriteLine);
-            //fPPl2.ToList().ForEach(Console.WriteLine);
+            var i = num.MyFirst();
+            Console.WriteLine(i);
             Console.ReadKey();
         }
-
-
     }
 }
