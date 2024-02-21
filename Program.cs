101516d2e794b62bee6cda38ff3f4b2e98454896
Riiyaz <45689131+abdulriiyaz@users.noreply.github.com>
2024-02-21 16:28:52 +0530

    Updated: ODD EVEN with Generics type params

--- Program.cs
+++ Program.cs
@@ -4,8 +4,36 @@
     {
         public static void Main(string[] args)
         {
+            var ll = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var odd = Filter<int>(ll, i => i % 2 != 0);
+
+            var even = Filter<int>(ll, i => i % 2 == 0);
+
+            Console.Write("ODD:");
+            foreach (var item in odd)
+            {
+                Console.WriteLine(item + " ");
+            }
+
+            Console.Write("EVEN:");
+            foreach (var item in even)
+            {
+                Console.WriteLine(item + " ");
+            }
 
             Console.ReadKey();
+        }
+
+        static IEnumerable<T> Filter<T>(IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
         }
 
 
@@ -14,9 +42,9 @@
         //method which returns instance of type T with the help of keyowrd 'where' and 'new()'
 
 
-        static T Hey<T>() where T : new()
-        {
-            return new T();
-        }
+        //static T Hey<T>() where T : new()
+        //{
+        //    return new T();
+        //}
     }
 }
