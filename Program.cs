16cdecf8d94e70f7a814ba3d55b824c2aceb8a1d
Riiyaz <45689131+abdulriiyaz@users.noreply.github.com>
2024-02-26 12:30:18 +0530

    Whosh

--- Program.cs
+++ Program.cs
@@ -1,39 +1,9 @@
-﻿using IOApplication.Classes;
-
-namespace IOApp
+﻿namespace IOApp
 {
     class Program
     {
-        static void GetUserInputs(Input inp_, GUI gui_)
-        {
-            Console.Write("Please enter a number: ");
-            int i = inp_.GetInt();
-
-            Console.Write("\nPlease enter a string: ");
-            string? s = inp_.GetString();
-
-            Console.Write("\nPlease enter yes or no.(y/n): ");
-            string? y = inp_.GetYesNo();
-
-            gui_.DisplayInput(inp_);
-
-        }
         public static void Main(string[] args)
         {
-            Input inp = new();
-            GUI gui = new();
-            FileIO file = new();
-
-            GetUserInputs(inp, gui);
-            try
-            {
-                file.WriteToFile("names.dat", inp);
-                file.ReadFromFile("names.dat");
-            }
-            catch (IOException err)
-            {
-                Console.WriteLine(err);
-            }
             Console.ReadKey();
         }
     }
